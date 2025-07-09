424716f3972cfc03ece8c7c78bb645110be83d35
Shmuye <shmuye27@gmail.com>
2025-07-09 08:45:34 +0300

    update the app.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import App from "./App";
 import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById('root'))
+import { StrictMode } from "react";
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
